002ac2d78838e133a0ad054dc252e1d6b34eb7dc
tmax818 <tylermaxwell661@gmail.com>
2022-12-13 07:24:45 -0800

    add rest

---
+++ src/main/java/tylermaxwell/HoppersReciptStsApplication.java
@@ -0,0 +1,13 @@
+package tylermaxwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersReciptStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersReciptStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tylermaxwell/HoppersReciptStsApplicationTests.java
@@ -0,0 +1,13 @@
+package tylermaxwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReciptStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
